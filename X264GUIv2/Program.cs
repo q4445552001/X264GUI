298ede309ea89d9144dfaff87949d0a37a6b8447
q4445552001 <42619163+q4445552001@users.noreply.github.com>
2026-05-06 20:49:03 +0800

    Add files via upload

--- X264GUIv2/Program.cs
+++ X264GUIv2/Program.cs
@@ -4,6 +4,13 @@ namespace X264GUIv2
 {
     //TODO: trim 還有問題
     //TODO: 路徑語系錯誤
+    //TODO: SQL改反射
+    //TODO: 顏色設定改初始化實做
+    //TODO: 進度條文字修正(超出範圍元範圍會被引截掉)
+    //TODO: 2PASS的進度有問題
+    //TODO: 儲存體只有一筆時無法儲存
+    //TODO: bitrate Auto時轉換結束後手動會被打開
+    //TODO: mkv的aac失效
 
     internal static class Program
     {
@@ -13,7 +20,14 @@ namespace X264GUIv2
         [STAThread]
         static void Main()
         {
-            string[] files = [@".\bin\ffmpeg\ffprobe.exe", @".\bin\ffmpeg\ffmpeg.exe"];
+            using var mutex = new Mutex(true, Application.ProductName, out var isFirstOpen);
+            if (!isFirstOpen && MessageBox.Show("程式已開啟", "提示") == DialogResult.OK)
+                Environment.Exit(1);
+
+            string[] files = [
+                @".\bin\ffmpeg\ffprobe.exe",
+                @".\bin\ffmpeg\ffmpeg.exe",
+            ];
 
             foreach (string file in files)
             {
